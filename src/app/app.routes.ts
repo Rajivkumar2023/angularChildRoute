eb385e309fe864805dd8cf4f63acca63d0e8230a
Rajiv kumar <rajivdhanama00@gmail.comuser.name>
2024-03-03 21:56:45 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CustomerComponent } from './customer/customer.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AdminComponent } from './admin/admin.component';
+import { ProductComponent } from './product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:"",pathMatch:'full'},
+    {path:"",component:HomeComponent},
+    {path:"customer",component:CustomerComponent},
+    {path:"contact-us",component:ContactUsComponent},
+    {path:'product',component:ProductComponent},
+    {path:"product",children:[
+        {path:'admin',component:AdminComponent}
+    ]},
+    {path:"",children:[
+        {path:'admin',component:AdminComponent}
+    ]}
+
+];
